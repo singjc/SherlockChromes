40e5bf3313b441b6a556dad3648f1fa319ad09b0
LLYX <leon.xu@mail.utoronto.ca>
2019-03-06 21:49:10 -0500

    Add baseline lstm model and training code

---
+++ models/baseline_lstm_model.py
@@ -0,0 +1,28 @@
+import torch
+import torch.nn as nn
+
+class BaselineChromatogramPeakDetector(nn.Module):
+
+    def __init__(self, input_dim, hidden_dim, output_size):
+        super(BaselineChromatogramPeakDetector, self).__init__()
+        self.hidden_dim = hidden_dim
+
+        self.lstm = nn.LSTM(input_dim, hidden_dim, bidirectional=True)
+
+        self.hidden2output = nn.Linear(hidden_dim * 2, output_size)
+        self.hidden = self.init_hidden()
+
+    def init_hidden(self):
+        return (torch.zeros(2, 1, self.hidden_dim),
+                torch.zeros(2, 1, self.hidden_dim))
+
+    def forward(self, chromatogram):
+        lstm_out, self.hidden = self.lstm(
+            chromatogram.view(len(chromatogram), 1, -1), self.hidden)
+        peak_space = self.hidden2output(lstm_out.view(len(chromatogram), -1))
+        peak_scores = torch.sigmoid(peak_space)
+
+        return peak_scores
+
+if __name__ == "__main__":
+    pass
---
+++ train/baseline_lstm_train.py
@@ -0,0 +1,70 @@
+import numpy as np
+import sys
+import torch
+import torch.nn as nn
+import torch.optim as optim
+
+sys.path.insert(0, '../models')
+sys.path.insert(0, '../datasets')
+
+from baseline_lstm_model import BaselineChromatogramPeakDetector
+from visualizer import plot_confusion_matrix
+
+if __name__ == "__main__":
+    chromatograms, labels = (
+        np.load('../../../data/working/skyline_exported_chromatograms.npy'),
+        np.load('../../../data/working/skyline_exported_labels.npy'))
+
+    model = BaselineChromatogramPeakDetector(1, 6, 1)
+    loss_function = nn.BCELoss()
+    optimizer = optim.SGD(model.parameters(), lr=0.1)
+
+    with torch.no_grad():
+        inputs = torch.from_numpy(chromatograms[0]).float()
+        peak_scores = model(inputs)
+
+        #Accuracy
+        peaks = torch.from_numpy(labels[0]).float()
+        output = peak_scores
+        output = (output > 0.5).float().view(len(peaks))
+        correct = (output == peaks).float().sum()
+        print("Accuracy: {:.3f}".format(correct/output.shape[0]))
+
+    epochs = 300
+    for epoch in range(epochs):  
+        # for i in range(len(chromatograms)):
+        for i in [18]:
+            chromatogram, peaks = chromatograms[i], labels[i]
+
+            model.zero_grad()
+
+            model.hidden = model.init_hidden()
+
+            chromatogram = torch.from_numpy(chromatogram).float()
+            peaks = torch.from_numpy(peaks).float()
+
+            peak_scores = model(chromatogram)
+
+            loss = loss_function(peak_scores.view(len(peaks)), peaks)
+            loss.backward()
+            optimizer.step()
+
+            #Accuracy
+            output = peak_scores
+            output = (output > 0.5).float().view(len(peaks))
+            correct = (output == peaks).float().sum()
+            print("Epoch {}/{}, Loss: {:.3f}, Accuracy: {:.3f}".format(
+                epoch + 1, epochs, loss.data.item(), correct/output.shape[0]))
+
+    with torch.no_grad():
+        inputs = torch.from_numpy(chromatograms[0]).float()
+        peak_scores = model(inputs)
+
+        #Accuracy
+        peaks = torch.from_numpy(labels[0]).float()
+        output = peak_scores
+        output = (output > 0.5).float().view(len(peaks))
+        correct = (output == peaks).float().sum()
+        print("Accuracy: {:.3f}".format(correct/output.shape[0]))
+        plot_confusion_matrix(peaks, output, classes=['noPeak', 'Peak'], normalize=False,
+                      title='Normalized confusion matrix')
