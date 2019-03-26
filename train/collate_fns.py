86f55fcf1d10cb83b24d9a17b3dd4e2f9d707923
LLYX <leon.xu@mail.utoronto.ca>
2019-03-26 00:51:49 -0400

    Remove subsection padding since should not require if munging correct

--- train/collate_fns.py
+++ train/collate_fns.py
@@ -63,7 +63,7 @@ class PadChromatogramsFor1DCNN(object):
 
         label_out_dims = (batch_size, max_label_len)
 
-        torch_labels = torch.zeros(batch_size, max_label_len)
+        padded_labels = torch.zeros(batch_size, max_label_len)
 
         for i, chromatogram in enumerate(chromatograms):
             length = chromatogram.size(1)
@@ -71,39 +71,6 @@ class PadChromatogramsFor1DCNN(object):
 
         for i, label in enumerate(labels):
             length = label.size(0)
-            torch_labels[i, 0:length] = label
+            padded_labels[i, 0:length] = label
 
-        return [padded_chromatograms, torch_labels]
-
-class PadChromatogramSubsectionsFor1DCNN(object):
-    """Pad chromatogram subsections in 1DCNN batch to same size.
-    Subsections shaped (D, N).
-    Labels shaped (N, )
-    """
-
-    def __call__(self, batch):
-        batch_size = len(batch)
-        chromatograms = [item[0] for item in batch]
-        labels = [item[1] for item in batch]
-
-        lengths = [chromatogram.size()[1] for chromatogram in chromatograms]
-
-        max_len = max(lengths)
-
-        channel_dim = chromatograms[0].size()[0]
-
-        out_dims = (batch_size, channel_dim, max_len)
-
-        padded_chromatograms = torch.zeros(*out_dims)
-
-        torch_labels = torch.zeros(batch_size)
-
-        for i, chromatogram in enumerate(chromatograms):
-            length = chromatogram.size(1)
-            padded_chromatograms[i, 0:channel_dim, 0:length] = chromatogram
-
-        for i, label in enumerate(labels):
-            torch_labels[i] = label
-
-        return [padded_chromatograms, torch_labels]
-    
+        return [padded_chromatograms, padded_labels]
