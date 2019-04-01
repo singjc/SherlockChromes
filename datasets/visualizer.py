27aba559db00e92b7d9562124ce9042bee56836c
LLYX <leon.xu@mail.utoronto.ca>
2019-04-01 15:33:48 -0400

    Add function to analyze model output for set of idx

--- datasets/visualizer.py
+++ datasets/visualizer.py
@@ -1,12 +1,61 @@
 import matplotlib.pyplot as plt
 import numpy as np
+import os
 import pandas as pd
+import sys
+import torch
 
 from sklearn.metrics import confusion_matrix
 
-from chromatograms_dataset import ChromatogramsDataset, ChromatogramSubsectionsDataset
+from chromatograms_dataset import ChromatogramsDataset, ChromatogramSubsectionsDataset, ChromatogramSubsectionsInMemoryDataset
 
-def plot_chromatogram(chromatogram, labels=None):
+sys.path.insert(0, '../models')
+
+from depthwise_1dcnn_model import ChromatogramPeakDetectorHybrid1DCNN
+
+def plot_whole_chromatogram(chromatogram, labels=None, bb_start=None, bb_end=None):
+    """
+    Args:
+        chromatogram: (len_of_chromatogram, num_dimensions)
+        labels: (len_of_chromatogram, )
+    """
+    # Convert from single multivariate time series
+    # back to separate single variate time series
+    # with shape (num_dimensions, len_of_chromatogram)
+    if chromatogram.shape[0] == max(chromatogram.shape):
+        chromatogram = chromatogram.T
+
+    traces, timepoints, intensities = [], [], []
+
+    for trace in range(len(chromatogram)):
+        for timepoint in range(len(chromatogram[trace])):
+            traces.append('chromatogram_' + str(trace))
+            timepoints.append(timepoint)
+            intensities.append(chromatogram[trace][timepoint])
+
+    df = pd.DataFrame(
+        {'trace': traces, 'timepoint': timepoints, 'intensity': intensities})
+
+    by_trace = df.groupby('trace')
+
+    for trace, group in by_trace:
+        plt.plot(group['timepoint'], group['intensity'], label=trace)
+
+    labels = list((labels >= 0.5))
+
+    for i in range(len(labels)):
+        if labels[i] == 1:
+            plt.axvline(i, color='r')
+            plt.axvline(i + 30, color='r')
+
+    if bb_start and bb_end:
+        plt.axvline(bb_start, color='b')
+        plt.axvline(bb_end, color='b')
+
+    plt.legend()
+    plt.show()
+
+def plot_chromatogram_subsection(chromatogram, labels=None):
     """
     Args:
         chromatogram: (len_of_chromatogram, num_dimensions)
@@ -40,11 +89,8 @@ def plot_chromatogram(chromatogram, labels=None):
         peak_start, peak_end = (
             labels.index(1) - 1, len(labels) - labels[::-1].index(1))
 
-        plt.axvline(peak_start, color='r')
-        plt.axvline(peak_end, color='r')
-
-    for i in range(0, len(chromatogram[0]), 10):
-        plt.axvline(i, color='b')
+        plt.axvline(peak_start, color='b')
+        plt.axvline(peak_end, color='b')
 
     plt.legend()
     plt.show()
@@ -103,21 +149,112 @@ def plot_confusion_matrix(y_true, y_pred, classes,
 
     return ax
 
+def analyze_model(dataset, model, root_dir, idx_filename, mode='val'):
+    counter1, counter2, counter3, counter4 = 0, 0, 0, 0
+    no_boxes, less_boxes, equal_boxes, more_boxes = [], [], [], []
+
+    idxs = np.loadtxt(os.path.join(root_dir, idx_filename))
+    for idx in idxs:
+        idx = int(idx)
+        chromatogram, labels = dataset[idx]
+        dims = chromatogram.shape
+        output = model(torch.from_numpy(
+                    np.asarray(chromatogram)).view(1, *dims).float())[0]
+
+        num_boxes = (labels == 1).sum()
+        num_boxes_predicted = (output > 0.5).sum()
+        if num_boxes_predicted == 0:
+            counter1+= 1
+            no_boxes.append(idx)
+        elif 0 < num_boxes_predicted < num_boxes:
+            counter2+= 1
+            less_boxes.append(idx)
+        elif num_boxes_predicted == num_boxes:
+            counter3+= 1
+            equal_boxes.append(idx)
+        elif num_boxes_predicted > num_boxes:
+            counter4+= 1
+            more_boxes.append(idx)
+
+    np.savetxt(os.path.join(root_dir, mode + '_no_boxes.txt'), np.array(no_boxes))
+    np.savetxt(os.path.join(root_dir, mode + '_less_boxes.txt', np.array(less_boxes))
+    np.savetxt(os.path.join(root_dir, mode + '_equal_boxes.txt', np.array(equal_boxes))
+    np.savetxt(os.path.join(root_dir, mode + '_more_boxes.txt', np.array(more_boxes))
+
+    print(counter1, counter2, counter3, counter4)
+
+def test_model(dataset, model, mode='whole'):
+    loop = True
+    labels = dataset.labels
+
+    if mode != 'whole':
+        positives = np.where(labels == np.int64(1))[0]
+        negatives = np.where(labels == np.int64(0))[0]
+
+    while loop:
+        retrieve = input('Retrieve: ')
+
+        idx = 0
+
+        if mode != 'whole':
+            if retrieve == 'positive':
+                idx = np.random.choice(positives)
+            elif retrieve == 'negative':
+                idx = np.random.choice(negatives)
+            else:
+                idx = int(float(retrieve))
+        else:
+            idx = int(float(retrieve))
+
+        chromatogram, true_label = dataset[idx]
+        
+        print('Chromatogram/subsection id: {}'.format(idx))
+
+        if mode != 'whole':
+            print('Chromatogram subsection label: {}'.format(true_label))
+
+        with torch.no_grad():
+            dims = chromatogram.shape
+            output = model(
+                torch.from_numpy(
+                    np.asarray(chromatogram)).view(1, *dims).float())[0]
+
+            print(output)
+
+            output = output.numpy()
+
+            if mode != 'whole':
+                plot_chromatogram_subsection(chromatogram, output)
+            else:
+                plot_whole_chromatogram(chromatogram, output, *dataset.get_bb(idx))
+
 
 if __name__ == "__main__":
-    # chromatograms = ChromatogramsDataset(
-    #     '../../../data/working/ManualValidation',
-    #     'chromatograms.csv',
-    #     'skyline_exported_labels.npy')
-    # plot_chromatogram(chromatograms[0][0], chromatograms[0][1])
+    # e.g. ../../../data/working/ManualValidation
+    root_dir = input('Dataset root dir: ')
+    # e.g. chromatograms.csv
+    chromatograms_filename = input('Dataset chromatograms csv: ')
+    # e.g. skyline_exported_labels.npy
+    labels_filename = input('Dataset labels npy: ')
+    dataset = ChromatogramsDataset(
+        root_dir,
+        chromatograms_filename,
+        labels_filename)
 
-    chromatograms = ChromatogramSubsectionsDataset(
-        '../../../data/working/ManualValidationSliced_20_1',
-        'chromatograms.csv')
+    # e.g. ../../../data/output/1dcnn_xcept_whole/1dcnn_model_80_loss=0.01623706.pth
+    model_filename = input('Model pth: ')
+    model = torch.load(model_filename)
+    model.to('cpu')
+    model.batch_size = 1
+    model.eval()
 
-    for i in range(len(chromatograms)):
-        print(i)
-        chromatogram, label = chromatograms[i]
+    # e.g. ../../../data/output/1dcnn_hybrid_whole
+    output_root_dir = input('Output root dir: ')
+    # e.g. val_idx.txt
+    idx_filename = input('Val Idx filename: ')
+    analyze_model(dataset, model, output_root_dir, idx_filename)
+    # e.g. test_idx.txt
+    idx_filename = input('Test Idx filename: ')
+    analyze_model(dataset, model, output_root_dir, idx_filename, mode='test')
 
-        if label == 1:
-            plot_chromatogram(chromatogram)
+    test_model(dataset, model)
