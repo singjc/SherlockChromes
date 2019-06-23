048c382e4ad77f785a46ce66b96890101336d850
LLYX <leon.xu@mail.utoronto.ca>
2019-06-23 00:47:24 -0400

    Enable train only mode by setting test proportion to 0

--- train/train.py
+++ train/train.py
@@ -178,6 +178,9 @@ def train(
 
     @trainer.on(Events.EPOCH_COMPLETED)
     def log_validation_results(trainer):
+        if kwargs['test_batch_proportion'] == 0.0:
+            return
+
         evaluator.run(val_loader)
         metrics = evaluator.state.metrics
         print("Validation Results - Epoch: {} Avg accuracy: {:.4f} Avg precision: {:.4f} Avg recall: {:.4f} Avg loss: {:.4f}"
@@ -197,6 +200,9 @@ def train(
 
     @trainer.on(Events.COMPLETED)
     def log_test_results(trainer):
+        if kwargs['test_batch_proportion'] == 0.0:
+            return
+            
         evaluator.run(test_loader)
         metrics = evaluator.state.metrics
         print("Test Results - Epoch: {} Avg accuracy: {:.4f} Avg precision: {:.4f} Avg recall: {:.4f} Avg loss: {:.4f}"
