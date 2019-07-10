93f9e88b6ab8ede76fc11c547594cdfa2955e138
LLYX <leon.xu@mail.utoronto.ca>
2019-07-09 23:25:26 -0400

    Reimplement 1d Squeeze Excitation

--- models/custom_layers_and_blocks.py
+++ models/custom_layers_and_blocks.py
@@ -46,6 +46,25 @@ class DepthSeparableConv1d(nn.Module):
 
         return out
 
+class SqueezeExcitation1d(nn.Module):
+    def __init__(self, in_channels, reduction_ratio=16):
+        super(SqueezeExcitation1d, self).__init__()
+        self.squeeze = nn.AdaptiveAvgPool1d(1)
+        self.excitation = nn.Sequential(
+            nn.Linear(in_channels, in_channels // reduction_ratio, bias=False),
+            nn.ReLU(),
+            nn.Linear(in_channels // reduction_ratio, in_channels, bias=False),
+            nn.Sigmoid()
+        )
+
+    def forward(self, x):
+        b, c, _ = x.size()
+        y = self.squeeze(x).view(b, c)
+        y = self.excitation(y).view(b, c, 1)
+        out = x * y.expand_as(x)
+
+        return out
+
 class ChannelGate1d(nn.Module):
     def __init__(self, in_channels, reduction_ratio=16):
         super(ChannelGate1d, self).__init__()
