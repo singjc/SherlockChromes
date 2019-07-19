e51aa47b3f2a53ea81f9463564af27636954dd01
LLYX <leon.xu@mail.utoronto.ca>
2019-07-18 23:35:18 -0400

    Update rpn backbone to new architecture

--- models/rpn1d/region_proposal_network_1d.py
+++ models/rpn1d/region_proposal_network_1d.py
@@ -32,38 +32,34 @@ class Backbone1d(nn.Module):
 
         self.encoder.append(
             nn.Sequential(
-                DepthSeparableConv1d(
+                AttendedDepthSeparableConv1d(
                     in_channels=in_channels,
                     out_channels=out_channels[0],
                     kernel_size=kernel_sizes[0],
                     padding=paddings[0],
-                    dilation=dilations[0]
+                    dilation=dilations[0],
+                    depthwise_attn_kernel_size=(kernel_sizes[0] + 4),
+                    reduction_ratio=(out_channels[0] // 2)
                 ),
                 nn.ReLU(),
-                nn.BatchNorm1d(out_channels[0]),
-                GlobalContextBlock1d(
-                    in_channels=out_channels[0],
-                    reduction_ratio=(out_channels[0] // 2)
-                )
+                nn.BatchNorm1d(out_channels[0])
             )
         )
 
         for i in range(1, len(out_channels)):
             self.encoder.append(
                 nn.Sequential(
-                    DepthSeparableConv1d(
+                    AttendedDepthSeparableConv1d(
                         in_channels=out_channels[i - 1],
                         out_channels=out_channels[i],
                         kernel_size=kernel_sizes[i],
                         padding=paddings[i],
-                        dilation=dilations[i]
+                        dilation=dilations[i],
+                        depthwise_attn_kernel_size=(kernel_sizes[i] + 4),
+                        reduction_ratio=(out_channels[i] // 2)
                     ),
                     nn.ReLU(),
-                    nn.BatchNorm1d(out_channels[i]),
-                    GlobalContextBlock1d(
-                        in_channels=out_channels[i],
-                        reduction_ratio=(out_channels[i] // 2)
-                    )
+                    nn.BatchNorm1d(out_channels[i])
                 )
             )
 
@@ -71,58 +67,67 @@ class Backbone1d(nn.Module):
 
         self.decoder.append(
             nn.Sequential(
-                DepthSeparableConv1d(
+                AttendedDepthSeparableConv1d(
                     in_channels=out_channels[-1],
                     out_channels=out_channels[-2],
                     kernel_size=kernel_sizes[-1],
                     padding=paddings[-1],
-                    dilation=dilations[-1]
+                    dilation=dilations[-1],
+                    depthwise_attn_kernel_size=(kernel_sizes[-1] + 4),
+                    reduction_ratio=(out_channels[-2] // 2)
                 ),
                 nn.ReLU(),
-                nn.BatchNorm1d(out_channels[-2]),
-                GlobalContextBlock1d(
-                    in_channels=out_channels[-2],
-                    reduction_ratio=(out_channels[-1] // 2)
-                )
+                nn.BatchNorm1d(out_channels[-2])
             )
         )
 
         for i in range(len(out_channels) - 2, 0, -1):
             self.decoder.append(
                 nn.Sequential(
-                    DepthSeparableConv1d(
+                    AttendedDepthSeparableConv1d(
                         in_channels=(2 * out_channels[i]),
                         out_channels=out_channels[i - 1],
                         kernel_size=kernel_sizes[i],
                         padding=paddings[i],
-                        dilation=dilations[i]
+                        dilation=dilations[i],
+                        depthwise_attn_kernel_size=(kernel_sizes[i] + 4),
+                        reduction_ratio=(out_channels[i - 1] // 2)
                     ),
                     nn.ReLU(),
-                    nn.BatchNorm1d(out_channels[i - 1]),
-                    GlobalContextBlock1d(
-                        in_channels=out_channels[i - 1],
-                        reduction_ratio=(out_channels[i] // 2)
-                    )
+                    nn.BatchNorm1d(out_channels[i - 1])
                 )
             )
 
         self.decoder.append(
             nn.Sequential(
-                DepthSeparableConv1d(
+                AttendedDepthSeparableConv1d(
                     in_channels=(2 * out_channels[0]),
                     out_channels=out_channels[0],
                     kernel_size=kernel_sizes[0],
                     padding=paddings[0],
-                    dilation=dilations[0]
+                    dilation=dilations[0],
+                    depthwise_attn_kernel_size=(kernel_sizes[0] + 4),
+                    reduction_ratio=(out_channels[0] // 2)
                 ),
                 nn.ReLU(),
-                nn.BatchNorm1d(out_channels[0]),
-                GlobalContextBlock1d(
-                    in_channels=out_channels[0],
-                    reduction_ratio=(out_channels[0] // 2)
-                )
+                nn.BatchNorm1d(out_channels[0])
             )
         )
+
+        self.classifier = nn.Sequential(
+                AttendedDepthSeparableConv1d(
+                    in_channels=out_channels[0],
+                    out_channels=1,
+                    kernel_size=1,
+                    stride=1,
+                    padding=0,
+                    dilation=1,
+                    bias=False,
+                    depth_multiplier=1,
+                    intermediate_nonlinearity=False
+                ),
+                nn.BatchNorm1d(1)
+            )
 
     def forward(self, sequence):
         intermediate_outs = []
