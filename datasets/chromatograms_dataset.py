e4ca3564ac65bc8a08674223e65b437a68c12069
LLYX <leon.xu@mail.utoronto.ca>
2019-10-03 23:31:57 -0400

    Implement preloading of all chromatograms

--- datasets/chromatograms_dataset.py
+++ datasets/chromatograms_dataset.py
@@ -8,13 +8,22 @@ from torch.utils.data import Dataset
 class ChromatogramsDataset(Dataset):
     """Whole Chromatograms dataset with point labels."""
 
-    def __init__(self, root_path, chromatograms, labels, transform=None):
+    def __init__(
+        self,
+        root_path,
+        chromatograms,
+        labels,
+        extra_path=None,
+        transform=None,
+        preload=True):
         """
         Args:
             root_path (string): Path to the root folder.
             chromatograms (string): Filename of CSV with chromatogram 
                 filenames.
             labels (string): Filename of the npy file with labels.
+            extra_path (string, optional): Path to folder containing additional
+                traces.
             transform (callable, optional): Optional transform to be applied
                 on a sample (e.g. padding).
         """
@@ -22,23 +31,62 @@ class ChromatogramsDataset(Dataset):
         self.chromatograms = pd.read_csv(os.path.join(self.root_dir,
                                          chromatograms))
         self.labels = np.load(os.path.join(self.root_dir, labels))
+        self.extra_dir = extra_path
         self.transform = transform
+
+        self.chromatogram_npy = None
+        if preload:
+            for i in range(len(self.chromatograms)):
+                if self.chromatogram_npy is not None:
+                    self.chromatogram_npy = np.vstack(
+                        (self.chromatogram_npy, self.load_chromatogram(i)))
+                else:
+                    self.chromatogram_npy = self.load_chromatogram(i)
     
     def __len__(self):
         return len(self.chromatograms)
 
     def __getitem__(self, idx):
         chromatogram_id = self.chromatograms.iloc[idx, 0]
-        chromatogram_name = os.path.join(
-            self.root_dir,
-            self.chromatograms.iloc[idx, 1]) + '.npy'
-        chromatogram = np.load(chromatogram_name).astype(float)
+        
+        if self.chromatogram_npy:
+            chromatogram = self.chromatogram_npy[chromatogram_id]
+        else:
+            chromatogram = self.load_chromatogram(idx)
+
         label = self.labels[chromatogram_id].astype(float)
 
         if self.transform:
             chromatogram, label = self.transform((chromatogram, label))
 
         return chromatogram, label
+
+    def load_npy(self, npy_names):
+        npy = np.load(npy_names[0]).astype(float)
+
+        if len(npy_names) > 1:
+            for i in range(1, len(npy_names)):
+                npy = npy.vstack((npy, np.load(npy_names[i]).astype(float)))
+
+        return npy
+
+    def load_chromatogram(self, idx):
+        chromatogram_name = os.path.join(
+            self.root_dir,
+            self.chromatograms.iloc[idx, 1]) + '.npy'
+
+        npy_names = [chromatogram_name]
+
+        if self.extra_dir:
+            extra_name = os.path.join(
+                self.extra_dir,
+                self.chromatograms.iloc[idx, 1]) + '_Extra.npy'
+
+            npy_names.append(extra_name)
+            
+        chromatogram = self.load_npy(npy_names)
+
+        return chromatogram
 
     def get_bb(self, idx):
         bb_start, bb_end = \
