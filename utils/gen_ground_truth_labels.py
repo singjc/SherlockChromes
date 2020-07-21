47c2f67ab36ad66bd0d6c1664a97937077d2da65
Justin Sing <justincsing@gmail.com>
2020-07-21 15:01:49 -0400

    [FEATURE] Added gen_ground_truth_labels.py to generate ground truth binary labels that map to the chromatogram_csv output from osw_parser.py

---
+++ utils/gen_ground_truth_labels.py
@@ -0,0 +1,53 @@
+import os
+import click
+import numpy as np
+import pandas as pd
+
+@click.command()
+@click.option( '--in_chrom_csv', '--in_chromatograms_csv', help='Chromatogram csv file containing filename column containing peptide sequence. This file should be derived from osw_parser.' )
+@click.option( '--in_truth', '--ground_truth_file', help='Single column ground truth file containing peptide sequences that you know are present. i.e. synthetic peptides.' )
+@click.option( '--out_file', '--out_file', default="ground_truth_based_binary_labels.npy", help='Numpy file to write binary labels to.' )
+def main( in_chromatograms_csv, ground_truth_file, out_file ):
+    """Generate a binary array of ground truth labels"""
+
+    click.echo( "in_chromatograms_csv: {0}\nground_truth_file: {1}".format(in_chromatograms_csv,ground_truth_file))
+
+    # Check files for supported formats
+    in_chromatograms_csv_filename, in_chromatograms_csv_ext = os.path.splitext( in_chromatograms_csv )
+    if  ( in_chromatograms_csv_ext != '.tsv' and in_chromatograms_csv_ext != '.csv' and in_chromatograms_csv_ext != '.txt' ):
+        raise ValueError( 'chromatogram csv file of type {} is not supported. Your chromatogram csv file type should be .tsv/.csv/.txt'.format(in_chromatograms_csv_ext) )
+
+    truth_filename, truth_ext = os.path.splitext( ground_truth_file )
+    if  ( truth_ext != '.tsv' and truth_ext != '.csv' and truth_ext != '.txt' ):
+        raise ValueError( 'ground truth file of type {} is not supported. Your ground truth file type should be .tsv/.csv/.txt'.format(truth_ext) )
+    
+    # Get file delimiters
+    if in_chromatograms_csv_ext == '.tsv' or in_chromatograms_csv_ext == '.txt' :
+        chrom_csv_delim = "\t"
+    else:
+        chrom_csv_delim = ","
+    
+    if truth_ext == '.tsv' or truth_ext == '.txt' :
+        truth_ext_delim = "\t"
+    else:
+        truth_ext_delim = ","
+
+    # Load inputs into pandas dataframe
+    chromatogram_df = pd.read_csv( in_chromatograms_csv, sep=chrom_csv_delim )
+    truth_df = pd.read_csv( ground_truth_file, sep=truth_ext_delim )
+
+    # Extract Filename columna and convert to a list
+    filename = chromatogram_df['Filename'].tolist()
+    # Convert truth_df to list
+    truth_list = truth_df.to_numpy().flatten().tolist()
+
+    # Identify which filenames contain peptide sequences from truth list
+    binary_labels = [ len(set(os.path.basename(current_filename).split("_")) & set(truth_list))>0 if  1 else 0 for current_filename in filename ]
+    binary_labels = np.array( binary_labels )
+
+    # Write labels to file
+    click.echo( "Writing binary labels to disk with shape: {0}.\nOutput file: {1}".format(np.shape(binary_labels), out_file) )
+    np.save( out_file, binary_labels.astype(np.int32) )
+
+if __name__ == '__main__':
+    main()
